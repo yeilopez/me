cd68a2cc18e0be549c18085b10e96673384dc505
yei lopez <30511041+yeilopez@users.noreply.github.com>
2024-06-18 13:00:34 -0300

    Web v.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,76 @@
-!function(e){"use strict";e('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function(){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){var t=e(this.hash);if((t=t.length?t:e("[name="+this.hash.slice(1)+"]")).length)return e("html, body").animate({scrollTop:t.offset().top},1e3,"easeInOutExpo"),!1}}),e(".js-scroll-trigger").click(function(){e(".navbar-collapse").collapse("hide")}),e("body").scrollspy({target:"#sideNav"})}(jQuery);
-
-  $(function () {
-    $(document).scroll(function () {
-      var $backchang = $(".chanback");
-      $backchang.toggleClass('scrolled', $(this).scrollTop() > $backchang.height());
-    });
-  });
-
-  const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-  const currentTheme = localStorage.getItem('theme');
-  
-  if (currentTheme) {
-      document.documentElement.setAttribute('data-theme', currentTheme);
-    
-      if (currentTheme === 'dark') {
-          toggleSwitch.checked = true;
-      }
-  }
-  
-  function switchTheme(e) {
-      if (e.target.checked) {
-          document.documentElement.setAttribute('data-theme', 'dark');
-      }
-      else {        document.documentElement.setAttribute('data-theme', 'light');
-      }    
-  }
-  
-  toggleSwitch.addEventListener('change', switchTheme, true);
-  
-  document.getElementById("checkbox").checked = true;
-
-$( window ).resize(function() {
-    if ($(window).width() < 992) {
-      $('#moveElement').appendTo('.b');
-    } else {
-      $('#moveElement').appendTo('.a');
-  }});
-
-window.onload = function() {
-    if ($(window).width() < 992) {
-        $('#moveElement').appendTo('.b');
-      } else {
-        $('#moveElement').appendTo('.a');
+!(function (e) {
+  "use strict";
+  e('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var t = e(this.hash);
+      if ((t = t.length ? t : e("[name=" + this.hash.slice(1) + "]")).length)
+        return (
+          e("html, body").animate(
+            { scrollTop: t.offset().top },
+            1e3,
+            "easeInOutExpo",
+          ),
+          !1
+        );
     }
+  }),
+    e(".js-scroll-trigger").click(function () {
+      e(".navbar-collapse").collapse("hide");
+    }),
+    e("body").scrollspy({ target: ".nav-menu" });
+})(jQuery);
+
+const toggleSwitch = document.querySelector(
+  '.theme-switch input[type="checkbox"]',
+);
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if (currentTheme === "light") {
+    toggleSwitch.checked = true;
+  }
 }
 
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+}
 
-  $('.toggle-menu').click (function(){
-    $(this).toggleClass('active');
-    $('#menu').toggleClass('open');
-  });
+toggleSwitch.addEventListener("change", switchTheme, true);
 
-  $('.main-nav ul li a').on("click", function (e) {
-    $('.toggle-menu').removeClass('active');
-    $('#menu').removeClass('open');
+document.getElementById("checkbox").checked = false;
+
+$(window).resize(function () {
+  if ($(window).width() < 992) {
+    $("#moveElement").appendTo(".b");
+  } else {
+    $("#moveElement").appendTo(".a");
+  }
+});
+
+window.onload = function () {
+  if ($(window).width() < 992) {
+    $("#moveElement").appendTo(".b");
+  } else {
+    $("#moveElement").appendTo(".a");
+  }
+};
+
+$(".toggle-menu").click(function () {
+  $(this).toggleClass("active");
+  $("#menu").toggleClass("open");
+});
+
+$(".main-nav ul li a").on("click", function (e) {
+  $(".toggle-menu").removeClass("active");
+  $("#menu").removeClass("open");
 });
